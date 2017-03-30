f242fa5c115450b99f8ae359273ba62d3091cd48
Mika Rinne <mika.rinne@oracle.com>
2017-03-30 21:03:35 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3333;
+var server = process.env.SERVER || '';
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/config/', function(req, res) {
+  res.send({ 'server': server });
+});
+
+app.listen(port, function() {
+  	console.log('server listening on port ' + port);
+});
---
+++ startNodemon.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export SERVER="ws://140.86.9.133:5555"
+nodemon server.js
